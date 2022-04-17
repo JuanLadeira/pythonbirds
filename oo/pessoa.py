47a40243a1c105867c8dc42f7ee73c9057eaea92
JuanLadeira <juan.fll@hotmail.com>
2022-04-17 16:45:04 -0300

    Criado atributos de intância de nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade = None):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return 'olá, meu bom.'
+
     pass
 
 
 if __name__ == '__main__':
     p = Pessoa()
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
